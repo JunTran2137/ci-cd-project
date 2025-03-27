74090271009691918c555465ceb4f8155a1d6b1c
JunTran2137 <152272253+JunTran2137@users.noreply.github.com>
2025-03-27 09:24:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from CI/CD!'));
-app.listen(3000, () => console.log('App running on port 3000'));
+
+app.get('/', (req, res) => {
+  res.send('Hello from AWS CI/CD!');
+});
+
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
